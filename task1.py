f0db4571f0850501682775e9d8361a180f910287
LLJ <zmj_private@163.com>
2019-10-07 18:38:35 +0100

    file reader

--- task1.py
+++ task1.py
@@ -1,2 +1,13 @@
-print(1)
-xu
+import re
+
+def fileReader(path):
+    content = []
+    with open(path, 'r', encoding='utf-8') as f:
+        for line in f.readlines(path):
+            content.extend(line)
+
+    f.close()
+    return content
+
+def preprocess_line(content):
+    content = re.match('//')
