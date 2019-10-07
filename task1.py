aa415b2866572ae6575cc73622d19c27a0e7a5b3
stella47xu <xu47stella@gmail.com>
2019-10-07 18:42:55 +0100

    files2

--- task1.py
+++ task1.py
@@ -1,2 +1,23 @@
-print(1)
-xu
+# def readfile(path):
+#     text=[]
+#     with open (path,'r',encoding='utf-8') as f:
+#         for lines in f.readlines(path):
+#             text.extend(lines)
+#     f.close()
+#     return text
+
+import re
+
+def fileReader(path):
+    content = []
+    with open(path, 'r', encoding='utf-8') as f:
+        for line in f.readlines(path):
+            content.extend(line)
+
+    f.close()
+    return content
+#
+# def preprocess_line(content):
+#     content = re.match('//')
+
+print(fileReader("C:\Users\xu47s\PycharmProjects\Assignment1\training.de"))
