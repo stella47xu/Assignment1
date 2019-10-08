a21ef4c7d77aac20392aaf7af2b94972e9c5699e
LLJ <zmj_private@163.com>
2019-10-08 17:08:56 +0100

    task1_1 convert all digits to 0

--- task1_1.py
+++ task1_1.py
@@ -1,13 +1,23 @@
 import re
-
+import string
 def fileReader(path):
     content = []
     with open(path, 'r', encoding='utf-8') as f:
-        for line in f.readlines(path):
-            content.extend(line)
+        for line in f.readlines():
+            content.append(line)
 
-    f.close()
     return content
 
 def preprocess_line(content):
-    content = re.match('//')
+    new_content = []
+    for lines in content:
+        lines = lines.lower()
+        rstr= r"[^0-9a-z\.]"
+        newlines = re.sub(rstr," ",lines)
+        # convert all digits to 0
+        digit_rstr = r'[0-9]+'
+        newlines = re.sub(digit_rstr,'0',newlines)
+        new_content.append(newlines)
+    return new_content
+
+print(preprocess_line(fileReader("training.de")))
