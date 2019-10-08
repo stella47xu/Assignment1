36d7529bf887ab7399e547984949a39449865c05
stella47xu <xu47stella@gmail.com>
2019-10-08 16:13:08 +0100

    files_1.1

--- task1.py
+++ task1.py
@@ -1,14 +1,21 @@
 import re
-
+import string
 def fileReader(path):
     content = []
     with open(path, 'r', encoding='utf-8') as f:
         for line in f.readlines():
             content.append(line)
 
-    f.close()
     return content
 
-# def preprocess_line(content):
-#     content = re.match('//')
-print(fileReader(".//training.en"))
+def preprocess_line(content):
+    new_content = []
+    for lines in content:
+        lines = lines.lower()
+        rstr = r" "
+        newlines = re.sub(rstr,"_", lines)
+        new_content.append(newlines)
+    return new_content
+
+print(preprocess_line(fileReader("training.de")))
+
