be14f544f34610ce8207aa33122d82d09e405827
LLJ <zmj_private@163.com>
2019-10-08 17:19:18 +0100

    task1_1 add comments

--- task1_1.py
+++ task1_1.py
@@ -1,17 +1,34 @@
 import re
 import string
+
 def fileReader(path):
+    '''
+    read files
+    :param path:
+    :return:
+    '''
     content = []
     with open(path, 'r', encoding='utf-8') as f:
+        # read data of files line by line
         for line in f.readlines():
             content.append(line)
 
     return content
 
 def preprocess_line(content):
+    '''
+    preprocess each line of files:
+    lowercase all characters;
+    convert all digits to 0;
+    remove characters that are not digits, space, '.' or are not in alphabet
+    :param content:
+    :return:
+    '''
     new_content = []
     for lines in content:
+        # convert all characters into lowercase
         lines = lines.lower()
+        # replace all characters to be removed by space
         rstr= r"[^0-9a-z\.]"
         newlines = re.sub(rstr," ",lines)
         # convert all digits to 0
@@ -20,4 +37,5 @@ def preprocess_line(content):
         new_content.append(newlines)
     return new_content
 
-print(preprocess_line(fileReader("training.de")))
+f_path = "test"
+print(preprocess_line(fileReader(f_path)))
