b280779eab0d7973a41bb7a04cfaf8e842a9733e
LLJ <zmj_private@163.com>
2019-10-10 16:08:53 +0100

    task1_1

--- task1/task1_1.py
+++ task1/task1_1.py
@@ -48,7 +48,17 @@ def preprocess_line(content):
         new_content.append(newlines)
     return new_content
 
-f_path = "../data/test"
-f_write_path = './test_output'
-stems = preprocess_line(fileReader(f_path))
-fileWriter(stems, f_write_path)
+de_path = "../data/training.de"
+de_write_path = './de_output'
+stems = preprocess_line(fileReader(de_path))
+fileWriter(stems, de_write_path)
+
+en_path = "../data/training.en"
+en_write_path = './en_output'
+stems = preprocess_line(fileReader(en_path))
+fileWriter(stems, en_write_path)
+
+es_path = "../data/training.es"
+es_write_path = './es_output'
+stems = preprocess_line(fileReader(es_path))
+fileWriter(stems, es_write_path)
